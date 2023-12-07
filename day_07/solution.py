7adcb950b15f69e1b24f8f7b7a8978396202e94d
Nikolai Krot <n.krot@drooms.com>
2023-12-08 00:38:23 +0100

    imlement day 07 p2 alternatively

--- day_07/solution.py
+++ day_07/solution.py
@@ -55,7 +55,7 @@ class JokerCard(Card):
 
 @dataclass
 class Hand:
-    cards: str
+    cards: List[Card]
     bid: int
     strength: int = None
     rank: int = 0
@@ -92,29 +92,85 @@ class Hand:
         """Compute hand strength based on the available cards and
         return it (without setting).
         """
+        counts = Counter(self.label)
+        c_diff = len(counts)
+        c_most_common = counts.most_common(1)[0][1]
+        # print(c_diff, counts)
+
         strength = 0
-        counts = Counter(card.label for card in self.cards)
-        # print(counts, len(counts))
-        if len(counts) == 1:
+        if c_diff == 1:
             strength = 7  # five of a kind
-        elif counts.most_common(1)[0][1] == 4:
+        elif c_most_common == 4:
             strength = 6  # four of a kind
-        elif len(counts) == 2 and counts.most_common(1)[0][1] == 3:
+        elif c_diff == 2 and c_most_common == 3:
             strength = 5  # full house
-        elif counts.most_common(1)[0][1] == 3:
+        elif c_most_common == 3:
             strength = 4  # three of a kind
-        elif len(counts) == 3:
+        elif c_diff == 3:
             strength = 3  # two pair
-        elif counts.most_common(1)[0][1] == 2:
+        elif c_most_common == 2:
             strength = 2  # one pair
-        elif len(counts) == 5:
+        elif c_diff == 5:
             strength = 1  # high card
         if DEBUG:
             print("Strength", strength, self.label(), self)
         return strength
 
+    @property
     def label(self):
         return "".join(card.label for card in self.cards)
+
+
+class JokerHand(Hand):
+    """A hand of cards with special Joker logic"""
+
+    def __init__(self, src = None, **kwargs):
+        if isinstance(self, type(src)):
+            super().__init__(cards=src.cards, bid=src.bid)
+            self.strength: int = src.strength
+            self.rank: int = src.rank
+        else:
+            # not tested
+            super().__init__(**kwargs)
+
+    def compute_strength(self) -> int:
+        """Compute hand strength based on the available cards and
+        return it (without setting). Joker card receives a special
+        treatment.
+        """
+        jokers, normal_cards = [], []
+        for card in self.cards:
+            if isinstance(card, JokerCard):
+                jokers.append(card)
+            else:
+                normal_cards.append(card)
+
+        counts = Counter(card.label for card in normal_cards)
+        c_diff = len(counts)
+        c_jks = len(jokers)
+        c_most_common = counts.most_common(1)[0][1] if c_diff else 0
+
+        if DEBUG:
+            print(c_diff, counts)
+
+        strength = 0
+        if c_diff == 1 or c_jks == 5:
+            strength = 7  # five of a kind
+        elif c_most_common == 4 - c_jks:
+            strength = 6  # four of a kind
+        elif c_diff == 2 and c_most_common == 3 - c_jks:
+            strength = 5  # full house
+        elif c_most_common == 3 - c_jks:
+            strength = 4  # three of a kind
+        elif c_diff == 3:
+            strength = 3  # two pair
+        elif c_most_common == 2 - c_jks:
+            strength = 2  # one pair
+        elif c_diff == 5 - c_jks :
+            strength = 1  # high card
+        if DEBUG:
+            print("Strength", strength, self.label(), self)
+        return strength
 
 
 def play_joker(hand: Hand):
@@ -124,6 +180,8 @@ def play_joker(hand: Hand):
 
     Here we try to find the best card the joker can be and set
     Hand.strength to the best value.
+
+    This is a very stupid approach.
     """
     max_strength = hand.compute_strength()
 
@@ -171,7 +229,22 @@ def solve_p1(hands: List[Hand]) -> int:
     return res
 
 
-def solve_p2(hands: List[Hand]) -> int:
+def solve_p2(*args) -> int:
+    return solve_p2_v2(*args)
+
+def solve_p2_v2(hands: List[Hand]) -> int:
+
+    for hand in hands:
+        hand.cards = [
+            JokerCard(card) if card.label == 'J' else card
+            for card in hand
+        ]
+    hands = [JokerHand(hand) for hand in hands]
+
+    return solve_p1(hands)
+
+
+def solve_p2_v1(hands: List[Hand]) -> int:
     """Solution to the 2nd part of the challenge"""
 
     # convert joker cards to a JokerCard that have a different behaviour
