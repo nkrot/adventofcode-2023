0ad9544b067f08b47caee82d98b1300f7ed6307a
Nikolai Krot <n.krot@drooms.com>
2023-12-11 11:05:53 +0100

    solve day 11 part 2

--- src/aoc/day_11/solution.py
+++ src/aoc/day_11/solution.py
@@ -18,6 +18,10 @@ DAY = '11'
 DEBUG = int(os.environ.get('DEBUG', 0))
 
 
+class Galaxy(Point):
+    pass
+
+
 def solve_part_1(fname: str):
     res = solve_p1(load_input(fname))
     print(res)
@@ -56,23 +60,54 @@ def stretch(mtx: Matrix) -> Matrix:
     dprint(f"--- Expanded ---\n{res.shape()}\n{res}")
     return res
 
-
-def solve_p1(mtx: Matrix) -> int:
-    """Solution to the 1st part of the challenge"""
-    galaxies = []
-    for xy, _ in stretch(mtx).findall(lambda v: v == '#'):
-        galaxies.append(Point(xy))
-    dprint("Galaxy coordinates", galaxies)
+def compute_distances(galaxies: List[Point]) -> List[int]:
     distances = []
     for i in range(len(galaxies)):
         for j in range(i+1, len(galaxies)):
             distances.append(galaxies[i].l1_dist(galaxies[j]))
-    return sum(distances)
+    return distances
+
+def solve_p1(args) -> int:
+    """Solution to the 1st part of the challenge"""
+    mtx, _ = args
+    galaxies = []
+    for xy, _ in stretch(mtx).findall(lambda v: v == '#'):
+        galaxies.append(Point(xy))
+    dprint("Galaxy coordinates", galaxies)
+    return sum(compute_distances(galaxies))
 
 
-def solve_p2(lines: List[str]) -> int:
+def solve_p2(args) -> int:
     """Solution to the 2nd part of the challenge"""
-    return 0
+    mtx, expansion_coefficient = args
+
+    galaxies = []
+    for xy, value in mtx:
+        if value == "#":
+            galaxies.append(Galaxy(*xy))
+    dprint("Galaxies 0", galaxies)
+
+    maxx, maxy = Point(mtx.shape()) - (1,1)
+    for x, row in enumerate(reversed(mtx.rows())):
+        if all(s == "." for s in row):
+            expand_universe(galaxies, (maxx-x, None) , expansion_coefficient-1)
+    dprint("Galaxies x", galaxies)
+
+    for y, column in enumerate(reversed(mtx.columns())):
+        if all(s == "." for s in column):
+            expand_universe(galaxies, (None, maxy-y) , expansion_coefficient-1)
+    dprint("Galaxies y", galaxies)
+
+    return sum(compute_distances(galaxies))
+
+
+def expand_universe(galaxies, xy, coefficient):
+    x, y = xy
+    for g in galaxies:
+        if x is not None and g.x > x:
+            g.x += coefficient
+        if y is not None and g.y > y:
+            g.y += coefficient
 
 
 def load_input(fname: str = None):
@@ -84,12 +119,17 @@ def load_input(fname: str = None):
 
 
 tests = [
-    (load_input('test.1.txt'), 374, None),
+    ((load_input('test.1.txt'), 1), 374, None),
+
+    # part 2
+    ((load_input('test.1.txt'), 2), None, 374),
+    ((load_input('test.1.txt'), 10), None, 1030),
+    ((load_input('test.1.txt'), 100), None, 8410),
 ]
 
 
 reals = [
-    (load_input(), 9723824, None)
+    ((load_input(), 1000000), 9723824, 731244261352)
 ]
 
 
